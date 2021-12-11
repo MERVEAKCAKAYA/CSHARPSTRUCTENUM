b9bab980f9ce1f8e43998f9ea8d73384c0e2266f
merve <1merveakcakaya@gmail.com>
2021-12-11 19:31:38 +0300

    my first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StructVeEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
